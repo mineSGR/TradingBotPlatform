2efba87e4f40e9e84c849693bc6193a43532c623
mineSGR <mineSGR@user.noreply.github.com>
2020-09-01 09:45:16 +0200

    lagt till en att göra list

--- src/bot/Shuffler.java
+++ src/bot/Shuffler.java
@@ -12,7 +12,7 @@ public class Shuffler extends Thread {
 		this.start();
 	}
 	
-	public void run() {
+	public void run() {//Check så att alla lock ligger rätt
 		parent.scout.lock.writeLock().lock();
 		try {
 			for(int j = 0; j < parent.scout.stocks.size(); j++) {
