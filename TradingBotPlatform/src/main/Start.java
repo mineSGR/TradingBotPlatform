f832bd7e47425df4ceb13a86c6c08517434f3edd
OST1Z <61254276+OST1Z@users.noreply.github.com>
2020-09-10 09:46:17 +0200

    Add files via upload

---
+++ TradingBotPlatform/src/main/Start.java
@@ -0,0 +1,175 @@
+package main;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.math.BigDecimal;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import bot.Crawler;
+import bot.TradingBot;
+
+public class Start {
+	
+	public static class stock {
+		
+		public String name;
+		public BigDecimal value;
+		public boolean newAktie;
+		
+		public stock(String name, BigDecimal value) {
+			this.name = name;
+			this.value = value;
+			newAktie = true;
+		}
+	}
+	
+	public static class user {
+		public TradingBot bot;
+		public String username;
+		public String password;
+		
+		public user(String username, String password) {
+			this.username = username;
+			this.password = password;
+			bot = new TradingBot();
+		}
+	}
+	
+	private static String map;
+	public static Crawler scout;
+	public static Shuffler dealer;
+	public static ArrayList<user> allUsers;
+	public static ReentrantReadWriteLock userListLock;
+	
+	public static void main(String[] args) {
+		load();
+		map = System.getenv("Appdata") + "\\SebbeProduktion\\TradingBot";
+		allUsers = new ArrayList<user>();
+		userListLock = new ReentrantReadWriteLock();
+		scout = new Crawler();
+		dealer = new Shuffler();
+		save();
+	}
+	
+	private static boolean nameExists(String name) {
+		userListLock.readLock().lock();
+		boolean found = false;
+		try {
+			for(int i = 0; i < allUsers.size(); i++) {
+				if(allUsers.get(i).username.equals(name)) {
+					found = true;
+				}
+			}
+		} finally {
+			userListLock.readLock().unlock();
+		}
+		return found;
+	}
+	
+	private static void newUser(String name) {
+		userListLock.writeLock().lock();
+		try {
+			
+		} finally {
+			userListLock.writeLock().unlock();
+		}
+	}
+	
+	private static void save() {
+		File file = new File(map);
+		if(!file.exists()) {
+			file.mkdirs();
+		}
+		file = new File(map + "\\SaveFile.txt");
+		if(!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (Throwable t) {
+				errorLogg(t.toString());
+			}
+		}
+		try {
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
+			userListLock.writeLock().lock();
+			try {
+				oos.writeObject(allUsers);
+			} finally {
+				userListLock.writeLock().unlock();
+			}
+			scout.lock.readLock().lock();
+			try {
+				oos.writeObject(scout.stocks);
+			} finally {
+				scout.lock.readLock().unlock();
+			}
+			oos.close();
+		} catch(Throwable t) {
+			errorLogg(t.toString());
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static void load() {
+		File file = new File(map);
+		if(!file.exists()) {
+			file.mkdirs();
+		}
+		file = new File(map + "\\SaveFile.txtt");
+		if(!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (Throwable t) {
+				errorLogg(t.toString());
+			}
+		}
+		try {
+			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+			ArrayList<user> tmpArrUser = (ArrayList<user>) ois.readObject();
+			ArrayList<ArrayList<stock>> tmpArrArrStock = (ArrayList<ArrayList<stock>>) ois.readObject();
+			if(tmpArrArrStock != null) {
+				scout.lock.writeLock().lock();
+				try {
+					scout.stocks = tmpArrArrStock;
+				} finally {
+					scout.lock.writeLock().unlock();
+				}
+			}
+			if(tmpArrUser != null) {
+				userListLock.writeLock().lock();
+				try {
+					allUsers = tmpArrUser;
+				} finally {
+					userListLock.writeLock().unlock();
+				}
+			}
+			ois.close();
+		} catch(Throwable t) {
+			errorLogg(t.toString());
+		}
+	}
+	
+	public static void errorLogg(String error) {
+		File file = new File(map);
+		if(!file.exists()) {
+			file.mkdirs();
+		}
+		file = new File(map + "\\Error.txt");
+		if(!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (Throwable t) {}
+		}
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(file, true));
+			bw.append("Error: " + error + "\n");
+			bw.close();
+		} catch (Throwable t) {}
+	}
+}
